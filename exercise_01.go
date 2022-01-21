34f64be4489098781a8703c9e6970e83dff71496
rockle2000 <quanghhuuyy000@gmail.com>
2022-01-21 15:59:25 +0700

    add waitgroup

--- exercise_01.go
+++ exercise_01.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	// "runtime"
+	"sync"
 	"time"
 )
 
@@ -11,18 +13,23 @@ func output(c chan int,name string){
 		time.Sleep(time.Second/100)
 		fmt.Println("go routine ",name," is running: ",i)
 	}
+	wg.Done()
 }
+var wg sync.WaitGroup
 func main() {
+	
 	in := make(chan int, NumberUrl)
 	for i := 0; i < NumberUrl; i++ {
 		in <- (i + 1)
 	}
 	close(in)
+	
+	wg.Add(5)
 	go output(in,"1")
 	go output(in,"2")
 	go output(in,"3")
 	go output(in,"4")
 	go output(in,"5")
-
-	time.Sleep(time.Second*5)
+	// fmt.Println("number: ",runtime.NumGoroutine())
+	wg.Wait()
 }
